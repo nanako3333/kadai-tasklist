020ad54a675e6b4b4a5a106b992ce2fad67ae4b4
nanakokaneta <rusuposu@gmail.com>
2017-04-26 01:37:00 +0000

    kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,14 +2,12 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy, :edit]  
+#  before_action :correct_user, only: [:destroy, :edit]  
   
   
   def index
-    @tasks = Task.all.page(params[:page])
-#@user = current_user
-#@task = current_user.tasklists.build  # form_for 用
-#@tasks = current_user.tasklists.order('created_at DESC').page(params[:page])
+#     @tasks = User.all.page(params[:page])
+    @tasks = current_user.tasks.page(params[:page])
   end
 
   def show
@@ -64,11 +62,11 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
-  def correct_user
-    @task = current_user.tasks.find_by(id: params[:id])
-    unless @task
-      flash[:danger] = 'このTaskは操作できません '
-      redirect_to root_path
-    end
-  end
+#  def correct_user
+#   @task = current_user.tasks.find_by(id: params[:id])
+#    unless @task
+#      flash[:danger] = 'このTaskは操作できません '
+#      redirect_to root_path
+#    end
+#  end
 end
